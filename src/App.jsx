63e68fe3b3d0df3daa58c334f091391a755ead1b
Johanna Monroy <johamonroy20@gmail.com>
2026-07-29 18:47:26 +0200

    feat: build JouOff landing page from Figma desing, remove Vite template saffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
+import LandingPage from './pages/LandingPage'
 import RegistroPage from './pages/RegistroPage'
 import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/NotFoundPage'
@@ -7,7 +7,7 @@ import NotFoundPage from './pages/NotFoundPage'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/registro" element={<RegistroPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="*" element={<NotFoundPage />} />
